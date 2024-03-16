a363398ac45d7495b2504a36a2d4738b41bd2a1e
Benjamin Hester <blhester@liberty.edu>
2024-03-16 16:53:02 -0400

    Changelist 3/16/2023 - 4:52 PM
    ### .gitignore:
     - Added .iml files to the ignore list
    
    ### mnist_dataset.py:
     - Added the ability to only save the images for images that are 0's when running in non-local env's
    
    ### neural_network_playground_test.go:
     - Added the ability to run dynamically on the folders placed in the tmp/images folder

--- datasets/mnist_dataset.py
+++ datasets/mnist_dataset.py
@@ -7,6 +7,7 @@ from datasets import load_dataset
 # dataset.save_to_disk('tmp/mnist')
 csv_file_path = '../csv_reader/output.csv'  # Specify the desired output CSV file path
 output_directory = 'tmp/images'  # Specify the desired output directory
+env: str = os.getenv('ENV', 'local')
 
 counter = 0
 if __name__ == "__main__":
@@ -14,15 +15,15 @@ if __name__ == "__main__":
     for idx, data in enumerate(dataset):
         counter += 1
         # Extract the image object and the label
-        image, label = data['image'], data['label']
+        image, label = data['image'], int(data['label'])
+        if env == 'local' or label == 0:
+            expected_directory = os.path.join(output_directory, f"{label}")
+            if not os.path.exists(expected_directory):
+                os.makedirs(expected_directory)
+            # Generate the output file path
+            # Naming pattern: "label_index.png" (e.g., "5_0.png" for the first image labeled as 5)
+            output_path = os.path.join(expected_directory, f"{counter}.png")
 
-        expected_directory = os.path.join(output_directory, f"{int(label)}")
-        if not os.path.exists(expected_directory):
-            os.makedirs(expected_directory)
-        # Generate the output file path
-        # Naming pattern: "label_index.png" (e.g., "5_0.png" for the first image labeled as 5)
-        output_path = os.path.join(expected_directory, f"{counter}.png")
-
-        # Save the image
-        image.save(output_path)
+            # Save the image
+            image.save(output_path)
     print("Images saved successfully.")
